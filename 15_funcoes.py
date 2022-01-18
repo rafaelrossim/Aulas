ae386355a743bfa27d4b6a6e3c7b0f847bf5b5b0
RafaelRossim <58317726+rafaelrossim@users.noreply.github.com>
2022-01-18 11:30:59 -0300

    update functions

--- 15_funcoes.py
+++ 15_funcoes.py
@@ -1,4 +1,4 @@
-"""
+
 #definindo uma função
 def hello():
     print("hello!!")
@@ -19,16 +19,7 @@ cantar_parabens()
 #utilizando varias vezes
 for n in range(5):
     cantar_parabens()
-
-"""
-
-def cantar_parabens():
-    print("Parabéns para você")
-    print("Nesta data querida")
-    print("Muitas felicidades")
-    print("Muitos anos de vida!!")
-    print("---------------------")
-
+    
 #utilizando a função em uma variável. Não é tão utitizável mas é possível
 canta = cantar_parabens #neste caso não precisa do parenteses
 
