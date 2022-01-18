0eace3549527adecf2e80264f2e234ad07c3b01d
RafaelRossim <58317726+rafaelrossim@users.noreply.github.com>
2022-01-18 19:51:02 -0300

    funcoes com retorno

--- 15_funcoes.py
+++ 15_funcoes.py
@@ -1,3 +1,4 @@
+"""
 #definindo uma função
 def hello():
     print("hello!!")
@@ -24,3 +25,46 @@ canta = cantar_parabens #neste caso não precisa do parenteses
 
 #executando a variavel que recebeu a função
 canta()
+
+#funcão com retorno
+def hello():
+    return('Oi ')
+
+#utilizando a funcao com retorno
+nome = input('Qual seu nome? ')
+print(hello() + nome + "!")
+
+#retornando varios resultados
+def nova_funcao():
+    var = True # neste caso vai retornar 4
+    #var = None # neste caso vai retornar 3.2
+    #var = False # neste caso vai retornar b
+    if var:
+        return 4
+    elif var is None:
+        return 3.2
+    return "b"
+
+print(nova_funcao())
+
+from random import random
+
+#exemplo 1
+def joga_moeda():
+    #gera um núero pseudo-randomico, ou seja pode se repetir, entre 0 e 1
+    valor = random()
+    if valor > 0.5:
+        return (f'{valor:.1}, Cara')
+    return (f'{valor:.1}, Coroa')
+
+print (joga_moeda())
+
+#exemplo 2
+def joga_outra_moeda():
+    if random() > 0.5:
+        return 'Cara'
+    return 'Coroa'
+
+print (joga_outra_moeda())
+"""
+
